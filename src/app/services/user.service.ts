b432d94cab616986c02f20c585b1775107b5f3fb
Prabhu <vprabhu1771@gmail.com>
2024-11-18 12:39:59 +0530

    User Service Logic added

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
+import { environment as env } from '../../environments/environment';
+
+import { User } from '../models/User.model';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private readonly ROUTE = "SuperHeros";
+
+  formData: User = new User();
+
+  list?: User[];
+
+  constructor(private http: HttpClient) { }
+
+  postSuperHero() {
+    return this.http.post(`${env.BASE_URL}/${this.ROUTE}`, this.formData);
+  }
+
+  putSuperHero() {
+    return this.http.put(`${env.BASE_URL}/${this.ROUTE}/${this.formData.id}`, this.formData);
+  }
+
+  deleteSuperHero(id: number) {
+    return this.http.delete(`${env.BASE_URL}/${this.ROUTE}/${id}`);
+  }
+
+  refreshList() {
+    this.http.get(`${env.BASE_URL}/${this.ROUTE}`)
+      .toPromise()
+      .then(res =>this.list = res as User[]);
+  }
 }
